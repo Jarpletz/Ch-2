6784336d0f82ca80fa4eb56fc4324724f52a2d35
Jarpletz <rothpletz5@gmail.com>
2021-08-31 20:12:44 -0400

    v

---
+++ HW Ch.2/NetIncomeCalculator.cpp
@@ -0,0 +1,18 @@
+// HW Ch.2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+float payRate;
+float monhr, TeuHr, WedHr, ThursHr, FriHr;
+
+
+int main()
+{
+	cout << "Weekly Net Income Calculator\n" << endl;
+
+	return 0; 
+}
+
+
