ae890e3e4d232917c858b7dca566cdaf97814d6f
Jarpletz <rothpletz5@gmail.com>
2021-09-03 13:42:14 -0400

    AS

--- HW problem 2/HW problem 2.cpp
+++ HW problem 2/HW problem 2.cpp
@@ -5,31 +5,22 @@
 using namespace std;
 
 int numb;
-int d1;
-int d2;
-int d3;
-int d4;
-int d5;
+char d1;
+char d2;
+char d3;
+char d4;
+char d5;
 
 
 int main()
 {
-	MARKER:cout << "Insert a 5-digit Int: ";
-	cin >> numb;
-	if (numb < 10000 || numb>=100000) {
-		cout << "ERROR: Number must be 5 Digits!" << endl;
-		goto MARKER;
-	}
-	d1 = numb / 10000;
-	cout << "\n Digit 1: " << d1;
-	d2 = numb / 1000-d1*10;
-	cout << "\n Digit 2: " << d2;
-	d3 = numb / 100 - (d1*100)-(d2*10);
-	cout << "\n Digit 3: " << d3;
-	d4 = numb / 10 - (d1 * 1000) - (d2 * 100) - (d3*10);
-	cout << "\n Digit 4: " << d4;
-	d5 = numb - (d1 * 10000) - (d2 * 1000) - (d3 * 100) - (d4 * 10);
-	cout << "\n Digit 5: " << d5;
+	cout << "Insert a 5-digit Int: ";
+	cin >>d1>>d2>>d3>>d4>>d5;
+	cout << d1 << endl;
+	cout << d2 << endl;
+	cout << d3 << endl;
+	cout << d4 << endl;
+	cout << d5 << endl;
 	return 0;
 }
 
